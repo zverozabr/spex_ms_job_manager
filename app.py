38b7b681cedbc046704b4c519fd4ddb302101034
Artem Zubkov <zubkov.artem@gene.com>
2021-09-07 10:04:59 +0700

    converted to micro service v2 segment

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-import glob
+from spex_common.config import load_config
+from spex_common.modules.database import db_instance
+from spex_common.models.Task import task
+import spex_common.modules.omeroweb as omeroweb
+from glob import glob
 import json
 import importlib.util
 import importlib
+import os
+from services.Utils import download_file, getAbsoluteRelative
+
+collection = 'tasks'
+
+
+def scripts_list():
+    scripts: list = []
+    folder = f'{os.getenv("DATA_STORAGE")}\\Scripts\\*\\'
+    for script_folder in glob(folder):
+        scripts += [os.path.basename(os.path.dirname(script_folder))]
+    return scripts
 
 
 def get_script_params(script: str = "", part: str = "", subpart: list = None):
     subpart = subpart if subpart else []
     result: list = []
-    for file in glob.glob(f'{script}/{part}.json', recursive=True):
+    for file in glob(f'{script}/{part}.json', recursive=True):
         data = json.load(open(file))
 
         if depends := data.get('depends_and_script'):
@@ -27,7 +43,7 @@ def get_script_params(script: str = "", part: str = "", subpart: list = None):
 
 def start_scenario(script: str = "", part: str = "", subpart: list = None, **kwargs):
     subpart = subpart if subpart else []
-    for file in glob.glob(f'{script}/{part}.json', recursive=True):
+    for file in glob(f'{script}/{part}.json', recursive=True):
         data = json.load(open(file))
 
         if depends := data.get('depends_and_script'):
@@ -136,29 +152,72 @@ def start_scenario(script: str = "", part: str = "", subpart: list = None, **kwa
 # params_res = get_script_params(
 #     script='segmentation',
 #     part='find_boundaries',
-#     subpart=None
+#     subpart=['stardist_cellseg']
 # )
 #
 # print(params_res)
 
+def get_task():
+    tasks = db_instance().select(collection, 'FILTER doc.status == 0 or doc.status == -1 and doc.content like @value '
+                                             'LIMIT 1 ', value='%empty%')
+    if len(tasks) == 1:
+        return task(tasks[0]).to_json()
+    else:
+        tasks = db_instance().select(collection, ' FILTER doc.status == 0 LIMIT 1  ')
+        return task(tasks[0]).to_json() if len(tasks) == 1 else None
+
 
 def get_script_structure(folder: str = None):
     result_data = {}
-    if folder:
-        folder = folder + '/'
-    for file in glob.glob(f'{folder}stages.json', recursive=True):
+    folder = f'{os.getenv("DATA_STORAGE")}\\Scripts\\{folder}\\'
+    for file in glob(f'{folder}stages.json', recursive=True):
         data = json.load(open(file))
         for key, value in data.items():
-            result = {}
-            for file_scr in glob.glob(f'{folder}*.json', recursive=True):
+            for file_scr in glob(f'{folder}*.json', recursive=True):
                 file_scr_data = json.load(open(file_scr))
                 if file_scr_data.get('stage') == key:
                     cur = result_data.get(key) if result_data.get(key) else []
                     result_data.update({key: cur + [file_scr_data]})
         result_data.update(stages=data)
+    return result_data
 
-    print(result_data)
 
-param_res = get_script_structure(folder='segmentation')
+def get_image_from_omero(a_task):
+    image_id = a_task['omeroId']
+    author = a_task.get('author').get('login')
+    job_id = a_task.get('parent')
+    task_id = a_task.get('id')
 
-print(param_res)
+    session = omeroweb.get(author)
+
+    if session is not None:
+        download_url = '/webclient/render_image_download/' + str(image_id) + '/?format=tif'
+        path = download_file(download_url, client=session, jobid=job_id, taskid=task_id)
+        return path
+
+
+def update_status(status, onetask):
+    search = 'FILTER doc._key == @value LIMIT 1'
+    db_instance().update(collection, {'status': status}, search, value=onetask['id'])
+
+
+def take_start_return_result():
+    a_task = get_task()
+    if a_task is not None:
+        # download image tiff
+        path = get_image_from_omero(a_task)
+
+        if path is None:
+            update_status(-1, a_task)
+            return None
+        if os.path.isfile(getAbsoluteRelative(path)):
+            a_task['params'].update(image_path=getAbsoluteRelative(path))
+            a_task['params']["script"] = f'{os.getenv("DATA_STORAGE")}\\Scripts\\{a_task["params"]["script"]}'
+            result = start_scenario(**a_task["params"])
+            print(result)
+
+
+if __name__ == '__main__':
+    load_config()
+    take_start_return_result()
+
