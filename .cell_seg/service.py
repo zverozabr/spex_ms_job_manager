6911cbf116f6aadda36a584f9d062e8ae8b14a7a
Artem Zubkov <zubkov.artem@gene.com>
2021-12-19 23:35:45 +0700

    start refactoring

--- .cell_seg/service.py
+++ .cell_seg/service.py
@@ -1,30 +1,19 @@
 import pickle
-from os import path as pt
-from sys import platform
+import os
 
 
 def get(name):
-    if platform != 'linux' and platform != 'linux2':
-        # folder = pt.basename(pt.dirname(name))
-        # filename = f'{folder}/{pt.basename(name).split(".")[0]}.pickle'
-        filename = str(name).replace('.py', '.pickle')
-    else:
-        filename = f'{name.split(".")[0]}.pickle'
+    filename, ext = os.path.splitext(name)
+    filename = f'{filename}.pickle'
+
     with open(filename, "rb") as infile:
         data = pickle.load(infile)
-        infile.close()
-
         return data
 
 
 def put(name, data):
-    if platform != 'linux' and platform != 'linux2':
-        folder = pt.basename(pt.dirname(name))
-        # filename = f'{folder}/{pt.basename(name).split(".")[0]}.pickle'
-        filename = str(name).replace('.py', '.pickle')
-    else:
-        filename = f'{name.split(".")[0]}.pickle'
+    filename, ext = os.path.splitext(name)
+    filename = f'{filename}.pickle'
 
-    outfile = open(filename, "wb")
-    pickle.dump(data, outfile)
-    outfile.close()
+    with open(filename, "wb") as outfile:
+        pickle.dump(data, outfile)
--- app.py
+++ app.py
@@ -1,42 +1,25 @@
-import itertools
-from sys import platform
-from spex_common.config import load_config
-from spex_common.modules.database import db_instance
-from spex_common.models.Task import task
-from spex_common.models.Job import job
-import spex_common.modules.omeroweb as omeroweb
 import pathlib
-from os import path, remove
+import json
+import os
+import pickle
+import subprocess
+from sys import platform
+from os import path
+from glob import glob
+from spex_common.config import load_config
+from spex_common.models.Task import task
+from spex_common.modules.database import db_instance
 from spex_common.modules.logging import get_logger
 from spex_common.modules.aioredis import send_event
 from spex_common.models.OmeroImageFileManager import (
     OmeroImageFileManager as FileManager,
 )
-from glob import glob
-import json
-import importlib
-import os
-from services.Utils import getAbsoluteRelative
-from services.Timer import every
-import sys
-import pickle
-import csv
-import numpy as np
-import subprocess
-import pandas
+from spex_common.services.Utils import getAbsoluteRelative
+from spex_common.services.Timer import every
 
-
-logger = get_logger(f"spex.ms-job-manager")
+logger = get_logger("spex.ms-job-manager")
 
 collection = "tasks"
-
-
-def scripts_list():
-    scripts: list = []
-    folder = f'{os.getenv("DATA_STORAGE")}\\Scripts\\*\\'
-    for script_folder in glob(folder):
-        scripts += [os.path.basename(os.path.dirname(script_folder))]
-    return scripts
 
 
 def run_subprocess(folder, part, data):
@@ -380,246 +363,5 @@ def take_start_return_result():
 
 
 if __name__ == "__main__":
-
     load_config()
     every(5, take_start_return_result)
-    # result = start_scenario(
-    #     folder=".cell_seg",
-    #     image_path="2.ome.tiff",
-    #     script=".cell_seg",
-    #     subpart=[
-    #         "background_substract",
-    #         "median_denoise",
-    #         "nlm_denoise",
-    #         "classicwastershed_cellseg",
-    #         "stardist_cellseg",
-    #         "remove_small_objects",
-    #         "remove_large_objects"
-    #     ],
-    #     part="feature_extraction",
-    #     channel_list=[0],
-    #     kernal=5,
-    #     _min=1,
-    #     _max=98.5,
-    #     threshold=0.5,
-    #     mpp=0.39,
-    #     diamtr=20,
-    #     ch=0,
-    #     top=20,
-    #     subtraction=1,
-    #     minsize=2,
-    #     maxsize=98,
-    #     dist=8,
-    #     scaling=1,
-    # )
-
-
-# every(5, take_start_return_result)
-# take_start_return_result()
-
-# result = start_scenario(
-#     folder='.cell_seg',
-#     image_path='2.ome.tiff',
-#     script=".cell_seg",
-#     subpart=[
-#                 'background_substract',
-#                 'median_denoise',
-#                 'nlm_denoise',
-#             ],
-#     part="stardist_cellseg",
-#     channel_list=[0],
-#     kernal=5,
-#     _min=1,
-#     _max=98.5,
-#     threshold=0.5,
-#     mpp=0.39,
-#     diamtr=20,
-#     ch=0,
-#     top=20,
-#     subtraction=1,
-#     minsize=2,
-#     maxsize=98,
-#     dist=8,
-#     scaling=1
-# )
-
-# result = start_scenario(
-#     script='segmentation',
-#     part='stardist_cellseg',
-#     image_path='2.ome.tiff',
-#     kernal=5,
-#     channel_list=[0, 2, 3],
-#     scaling=1,
-#     threshold=0.5,
-#     _min=1,
-#     _max=98.5
-# )
-
-# result = start_scenario(
-#          script='segmentation',
-#          part='deepcell_segmentation',
-#          image_path='2.ome.tiff',
-#          channel_list=[0, 2, 3],
-#          kernal=5,
-#          mpp=0.39)
-
-# result = start_scenario(
-#     script='segmentation',
-#     part='rescues_cells',
-#     image_path='2.ome.tiff',
-#     channel_list=[0, 2, 3],
-#     kernal=5,
-#     mpp=0.39)
-
-
-# result = start_scenario(
-#     script='segmentation',
-#     part='feature_extraction',
-#     image_path='2.ome.tiff',
-#     channel_list=[0, 2, 3],
-#     kernal=5,
-#     mpp=0.39,
-#     dist=8)
-
-
-# result = start_scenario(
-#     script='segmentation',
-#     part='find_boundaries',
-#     subpart=['stardist_cellseg'],
-#     image_path='2.ome.tiff',
-#     channel_list=[0, 2, 3],
-#     threshold=0.5,
-#     _min=1,
-#     _max=98.5,
-#     scaling=1,
-#     kernal=5,
-#     mpp=0.39,
-#     dist=8)
-#
-# print(result)
-
-
-# params_res = get_script_params(
-#     script='segmentation',
-#     part='find_boundaries',
-#     subpart=['stardist_cellseg']
-# )
-#
-# print(params_res)
-
-
-# result = start_scenario(
-#     script='cell_seg',
-#     part='nlm_denoise',
-#     and_scripts=['median_denoise', 'background_subtract'],
-#     # image_path='2.ome.tiff',
-#     folder=".cell_seg",
-#     channel_list=[0, 2, 3],
-#     threshold=0.5,
-#     _min=1,
-#     _max=98.5,
-#     scaling=1,
-#     image_id=1,
-#     kernal=5,
-#     mpp=0.39,
-#     ch=0,
-#     top=5,
-#     subtraction=10,
-#     dist=8)
-
-#
-# result = start_scenario(
-# script='cell_seg',
-# part='feature_extraction',
-# subpart=[
-#     'stardist_cellseg',
-#     'median_denoise'
-# ],
-# and_scripts=[
-#     # 'remove_small_objects',
-#     'background_subtract',
-#     # 'remove_large_objects',
-#     'rescues_cells'
-# ],
-# folder='.cell_seg',
-# image_path='2.ome.tiff',
-# channel_list=[0, 2, 3],
-# scaling=1,
-# kernal=5,
-# _min=1,
-# _max=98.5,
-# threshold=0.5,
-# mpp=0.39,
-# ch=0,
-# top=20,
-# subtraction=1,
-# minsize=2,
-# maxsize=98,
-# dist=8)
-# if __name__ == '__main__':
-#
-#     df = "cell_seg_source.csv"
-#     with open(df, 'r') as f:
-#         reader = csv.reader(f, delimiter=',')
-#         headers = next(reader)
-#         df = np.array(list(reader)).astype(float)
-#
-# result = start_scenario(
-#     script="clustering",
-#     part="transformation",
-#     folder="clustering",
-#     df=df,
-#     markers=[1, 2, 3, 4],
-# )
-#
-#
-# result = start_scenario(
-#     script="clustering",
-#     part="zscore",
-#     folder="clustering",
-#     transformed=result['transformed'],
-#     markers=result['markers']
-# )
-#
-#
-# result = start_scenario(
-#     script="clustering",
-#     part="cluster",
-#     folder="clustering",
-#     **result,
-#     knn=30,
-#     df=df,
-# )
-#
-# result = start_scenario(
-#     script="clustering",
-#     part="dml",
-#     folder="clustering",
-#     min_dist=0.3,
-#     **result
-# )
-#
-# outfile = open('pickle.result', "wb")
-# pickle.dump(result, outfile)
-# outfile.close()
-
-# with open('pickle.result', "rb") as outfile:
-#     current_file_data = pickle.load(outfile)
-#     result = {**current_file_data}
-#
-#     result.update(bin_size=30, cluster_id_column=4, x_columns=[1, 2])
-#     outfile = open('qfmatch.pickle', "wb")
-#     pickle.dump(result, outfile)
-#     outfile.close()
-#
-# result = start_scenario(
-#     script="clustering",
-#     part="qfmatch",
-#     folder="clustering",
-#     bin_size=30,
-#     cluster_id_column=4,
-#     x_columns=[1, 2],
-#     **result
-# )
-#
-# print(result)
