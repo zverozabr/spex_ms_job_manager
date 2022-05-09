ed4768398e878da523105124bda8b3b387e6db10
Artem Zubkov <zubkov.artem@gene.com>
2022-05-09 20:06:39 +0700

    fixed some errors

--- models/ArangoWorker.py
+++ models/ArangoWorker.py
@@ -8,7 +8,7 @@ from spex_common.modules.database import db_instance
 from spex_common.modules.logging import get_logger
 from spex_common.services.Timer import every
 
-from .Constants import collection, EVENT_TYPE
+from models.Constants import collection, EVENT_TYPE
 from utils import (
     add_history as add_history_original,
     update_status as update_status_original
@@ -21,7 +21,7 @@ update_status = partial(update_status_original, collection, 'job_manager_catcher
 def get_task():
     tasks = db_instance().select(
         collection,
-        "FILTER doc.status == 0 or doc.status == -1 and doc.content like @value "
+        "FILTER doc.status == 0 or (doc.status == -1 and doc.content like @value) "
         "LIMIT 1 ",
         value="%empty%",
     )
@@ -40,7 +40,7 @@ def worker(name):
         if a_task := get_task():
             update_status(1, a_task)
             send_event(EVENT_TYPE, {"task": a_task})
-            logger.info(f'founded task send it to in work: {a_task.get("name")} / {a_task.get("key")}')
+            logger.info(f'found a task, sent it to in work: {a_task.get("name")} / {a_task.get("id")}')
 
     try:
         logger.info('Starting')
