c21b8a0aa36acb7d90bf8b6226e2cb730936e5cc
Artem Zubkov <zubkov.artem@gene.com>
2022-07-19 10:32:35 +0700

    added scripts for debug inside docker"

---
+++ app_for_start.py
@@ -0,0 +1,34 @@
+from multiprocessing import freeze_support
+from spex_common.config import load_config
+from spex_common.modules.logging import get_logger
+from models.Worker import Worker, get_pool_size
+from models.ArangoWorker import Worker as ArangoWorker
+
+
+def start_workers():
+    logger = get_logger('spex.ms-job-manager')
+    logger.info('Starting')
+    workers = []
+
+    worker = ArangoWorker(0)
+    workers.append(worker)
+    worker.start()
+
+    for index in range(get_pool_size('WORKERS_POOL')):
+        worker = Worker(index)
+        workers.append(worker)
+        worker.start()
+
+    try:
+        for worker in workers:
+            worker.join()
+    except KeyboardInterrupt:
+        pass
+
+    logger.info('Finished')
+
+
+if __name__ == "__main__":
+    freeze_support()
+    load_config()
+    start_workers()
---
+++ app_timer.py
@@ -0,0 +1,9 @@
+from spex_common.services.Timer import every
+
+
+def prri():
+    print("asdasdasd")
+
+
+if __name__ == "__main__":
+    every(5, prri)
